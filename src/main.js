0b6d66b7b316afa661cca1f2d72f09c1c983ce71
Darrell Malone Jr <darrell@dkmalone.com>
2024-07-12 16:03:15 -0500

    Consilidates /login into the index route.

--- src/main.js
+++ src/main.js
@@ -65,37 +65,9 @@ const oauthValid = (oauthObject) => {
     }
 };
 
-//Executing #1 after navigating to http://localhost:3333/login, a link is generated to redirect the user to auth.eagleeyenetworks.com so that they can log into their Eagle Eye developer account
-app.get('/login', (req, res) => {
-    let endpoint = "https://auth.eagleeyenetworks.com/oauth2/authorize";
-    let requestAuthUrl = `${endpoint}?client_id=${clientId}&response_type=code&scope=vms.all&redirect_uri=http://${hostName}:${port}`;
 
-    const page = `
-      <html>
-        <head><title>OAuth Testing(Node.JS)</title></head>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
-        <body>
-            <main style="display: flex; justify-content: center; height: 100vh;"> 
-                <div class="card" style="width: 55%; margin: 5rem; text-align:center;">
-                    <img src="https://upload.wikimedia.org/wikipedia/en/4/46/Eagle_Eye_Networks_logo.png" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h2 class="card-title">OAuth Testing(Node.JS)</h2>
-                        <p class="card-text mb-5">This sample Express application demonstrates how to authenticate and obtain an access token using the OAuth2 protocol for the Eagle Eye Networks REST API. To run this application, you will need to have a valid Eagle Eye Networks account and an application registered with the Eagle Eye Networks Developer Program. For more information on how to register an application, review the [API documentation](https://developer.eagleeyenetworks.com/docs/getting-started).</p>
-                        <a href='${requestAuthUrl}' class="btn btn-primary" style="margin-top: 5rem;">Login with Eagle Eye Networks</a>
-                    </div>
-                </div>
-            </main>
-
-          <script src="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
-        </body>
-      </html>
-    `;
-    res.send(page);
-  });
-
-  // Execution #2 after logging in the user is redirected back to the server with a code parameter in the query string
-    // If a user visits localhost:3333/login this route will be called.
-    // Execution #1 up above will generate a link that will redirect the user to auth.eagleeyenetworks.com.
+// Executing #1 after navigating to http://localhost:3333/, a link is generated to redirect the user to auth.eagleeyenetworks.com so that they can log into their Eagle Eye developer account
+// Executing #2 after logging in the user is redirected back to the server with a code parameter in the query string
 app.get('/', async (req, res) => {
     const code = req.query.code;
     if (code) {
@@ -125,7 +97,31 @@ app.get('/', async (req, res) => {
             res.status(500).send(error.toString());
         }
     } else {
-        res.send('The value of the code parameter is missing.');
+        // If no code is provided, the server will display a link to allow the user to log in.
+        let endpoint = "https://auth.eagleeyenetworks.com/oauth2/authorize";
+        let requestAuthUrl = `${endpoint}?client_id=${clientId}&response_type=code&scope=vms.all&redirect_uri=http://${hostName}:${port}`;
+    
+        const page = `
+          <html>
+            <head><title>OAuth Testing(Node.JS)</title></head>
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+            <body>
+                <main style="display: flex; justify-content: center; height: 100vh;"> 
+                    <div class="card" style="width: 55%; margin: 5rem; text-align:center;">
+                        <img src="https://upload.wikimedia.org/wikipedia/en/4/46/Eagle_Eye_Networks_logo.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h2 class="card-title">OAuth Testing(Node.JS)</h2>
+                            <p class="card-text mb-5">This sample Express application demonstrates how to authenticate and obtain an access token using the OAuth2 protocol for the Eagle Eye Networks REST API. To run this application, you will need to have a valid Eagle Eye Networks account and an application registered with the Eagle Eye Networks Developer Program. For more information on how to register an application, review the [API documentation](https://developer.eagleeyenetworks.com/docs/getting-started).</p>
+                            <a href='${requestAuthUrl}' class="btn btn-primary" style="margin-top: 5rem;">Login with Eagle Eye Networks</a>
+                        </div>
+                    </div>
+                </main>
+    
+              <script src="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
+            </body>
+          </html>
+        `;
+        res.send(page);
     }
 });
 

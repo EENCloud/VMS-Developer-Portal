f858e668b4d3a31345984c2255899a078005078d
Oscar Perez-Hernandez <perezoscar360@gmail.com>
2024-07-31 22:50:54 -0500

    corrected a console.log

--- OAuth-Node.JS/src/main.js
+++ OAuth-Node.JS/src/main.js
@@ -124,5 +124,5 @@ app.get('/', async (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log('Server is running on http://localhost:${port}')
+    console.log(`Server is running on http://localhost:${port}`)
 });

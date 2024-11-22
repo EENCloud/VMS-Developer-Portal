9745bf10811d18c31d456cc652a57869767a9841
Darrell Malone Jr <darrell@dkmalone.com>
2024-11-22 16:15:42 -0600

    IP Speaker Sample Code - Return the talkdown link for a camera - PLay a pre-recorded sound on the speaker

---
+++ IP Speakers/app/routes.py
@@ -0,0 +1,200 @@
+from app import app
+import os
+import json
+import urllib.parse
+
+from functools import wraps
+from app.een_client import (
+    EENClient,
+    AuthenticationError
+)
+from flask import (
+    request, render_template, session,
+    redirect, url_for, jsonify
+)
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Load the API Client
+client = EENClient(
+    os.getenv('CLIENT_ID'),
+    os.getenv('CLIENT_SECRET')
+)
+
+
+# Get the unquoted argument
+def get_unquoted_arg(arg_name):
+    value = request.args.get(arg_name)
+    return urllib.parse.unquote(value) if value else None
+
+
+# Check if the user is authenticated
+def is_authenticated():
+    return session.get('access_token') is not None
+
+
+def auth_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if not is_authenticated():
+            return redirect(url_for('login'))
+        return f(*args, **kwargs)
+    return decorated_function
+
+
+# Play Audio
+# This route plays audio to the selected camera
+@app.route('/talkdown/<camera_id>', methods=['POST'])
+@auth_required
+def play_audio(camera_id):
+    print('Retreiving Audio Push URL')
+    try:
+        response = client.play_audio(camera_id)
+    except Exception as e:
+        print(f"Failed to get the url: {e}")
+        return jsonify({
+            "status": "fail"
+        }), 500
+    if response:
+        return jsonify({
+            "status": "success"
+        }), 200
+    else:
+        return jsonify({
+            "status": "fail"
+        }), 500
+
+
+# Live View
+# This page will display high res live view of a selected camera.
+@app.route('/view/<camera_id>')
+@auth_required
+def view(camera_id):
+    media = {
+        'access_token': session.get('access_token'),
+        'base_url': session.get('base_url')
+    }
+
+    print('Pulling Feeds and Camera Info')
+    try:
+        response = client.get_cameras(camera_id)
+        camera = json.loads(response)
+    except Exception as e:
+        print(f"Failed to get camera info: {e}")
+
+    try:
+        response = client.get_feeds(
+            camera_id, type="main", include="multipartUrl,hlsUrl")
+        r_json = json.loads(response)
+        results = r_json['results']
+        print(f"Feeds: {results[0]}")
+    except Exception as e:
+        print(f"API Call failed: {e}")
+
+    try:
+        return render_template(
+            'view.html',
+            camera=camera,
+            media=media,
+            results=results[0])
+    except Exception as e:
+        print(f"Failed to render live view: {e}")
+
+
+# Index Page
+# This page will display a list of cameras for the user to select from.
+# If the user is not logged in, they will be redirected to the login page.
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    code = request.args.get('code')
+    if code:
+        return redirect(url_for('login') + '?code=' + code)
+    if not is_authenticated():
+        return redirect(url_for('login'))
+
+    media = {
+        'access_token': session.get('access_token'),
+        'base_url': session.get('base_url')
+    }
+
+    print('Pulling Camera List')
+    try:
+        cam_response = json.loads(client.get_cameras())
+        feed_response = json.loads(client.get_feeds())
+        cam_results = cam_response['results']
+        feed_results = feed_response['results']
+    except AuthenticationError:
+        return redirect(url_for('login'))
+    except Exception as e:
+        print(f"Failed to get cameras: {e}")
+        return render_template('index.html')
+
+    camera_dict = {camera['id']: camera for camera in cam_results}
+    for feed in feed_results:
+        camera_id = feed['deviceId']
+        if camera_id in camera_dict:
+            cam = camera_dict[camera_id]
+            if 'multipartUrl' not in cam:
+                cam['multipartUrl'] = feed['multipartUrl']
+
+    cameras = list(camera_dict.values())
+
+    try:
+        return render_template(
+            'index.html',
+            cameras=cameras[0:12],
+            media=media)
+    except Exception as e:
+        print(f"Failed render template: {e}")
+        return render_template('index.html')
+
+
+# Login Page
+# This page will redirect the user to the Eagle Eye Networks OAuth login page.
+# Review the OAuth Python example for more information.
+@app.route('/login')
+def login():
+    code = request.args.get('code')
+
+    if (code):
+        print("Attempting Code Auth")
+        auth_response = client.auth_een(code)
+        if auth_response.status_code == 200:
+            auth_response = json.loads(auth_response.text)
+
+            # Store the access_token, refresh_token,
+            # and base_url in the session
+            session['access_token'] = auth_response['access_token']
+            session['refresh_token'] = auth_response['refresh_token']
+            session['base_url'] = auth_response['httpsBaseUrl']['hostname']
+            session.permanent = True
+
+            return redirect(url_for('index'))
+        else:
+            print("Code Auth failed. {status_code} Response: {text}".format(
+                status_code=auth_response.status_code,
+                text=auth_response.text
+            ))
+
+    requestAuthUrl = "https://auth.eagleeyenetworks.com/oauth2/authorize"
+    params = {
+        "client_id": os.getenv('CLIENT_ID'),
+        "response_type": "code",
+        "scope": "vms.all",
+        "redirect_uri": "http://{host}:{port}".format(
+            host=os.getenv('FLASK_RUN_HOST'),
+            port=os.getenv('FLASK_RUN_PORT')
+        )
+    }
+    requestAuthUrl += '?' + urllib.parse.urlencode(params)
+
+    return render_template('login.html', auth_url=requestAuthUrl)
+
+
+# Logout Page
+# This page will clear the session and redirect the user to the login page.
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('login'))
